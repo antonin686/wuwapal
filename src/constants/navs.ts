d53109d741e2d7d717a3b4c0a41e5cde5c9761ec
Antonin Islam <antonin686.dev@gmail.com>
2025-02-04 03:01:18 +0600

    feat: adds dummy pages for all routes + update sidebar icons

--- src/constants/navs.ts
+++ src/constants/navs.ts
@@ -1,43 +1,56 @@
-import { Calendar, Combine, Sparkles, Type as LucideIcon, Settings } from "lucide-svelte";
+import {
+	Type as LucideIcon,
+	Sparkles,
+	Settings,
+	Swords,
+    CalendarRange,
+    Globe
+} from 'lucide-svelte';
 
 interface NavItem {
-    title: string;
-    path: string;
-    match: string;
-    icon: typeof LucideIcon;
+	title: string;
+	path: string;
+	match: string;
+	icon: typeof LucideIcon;
 }
 
 interface NavCategory {
-    [key: string]: NavItem[];
+	[key: string]: NavItem[];
 }
 
 export const NAVS: NavCategory = {
-    Convene: [
+	Convene: [
+		{
+			title: 'Convene Tracker',
+			path: '/convene',
+			match: '/convene',
+			icon: Sparkles
+		},
         {
-            title: "Convene Tracker",
-            path: "/convene",
-            match: "/convene",
-            icon: Sparkles
-        },
-        {
-            title: "Collector's Hub",
-            path: "/collectors-hub",
-            match: "/collectors-hub",
-            icon: Combine
-        },
-    ],
-    Planner: [
-        {
-            title: "Timeline",
-            path: "/timeline",
-            match: "/timeline",
-            icon: Calendar
-        },
-        {
-            title: "Settings",
-            path: "/settings",
-            match: "/settings",
-            icon: Settings
-        },
-    ]
+			title: "Global Stats",
+			path: '/global-stats',
+			match: '/global-stats',
+			icon: Globe
+		},
+		{
+			title: "Collector's Hub",
+			path: '/collectors-hub',
+			match: '/collectors-hub',
+			icon: Swords
+		}
+	],
+	Planner: [
+		{
+			title: 'Timeline',
+			path: '/timeline',
+			match: '/timeline',
+			icon: CalendarRange
+		},
+		{
+			title: 'Settings',
+			path: '/settings',
+			match: '/settings',
+			icon: Settings
+		}
+	]
 };
